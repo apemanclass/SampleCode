5e1e2f96e1f6c7ea3e788cccd08dc3bdcf3845c7
jiazhui <apemanclass@qq.com>
2022-10-10 14:00:42 +0800

    shape库集成demo

--- sampledemo/src/main/java/com/sample/yl/sampledemo/shape/ShapeActivity.java
+++ sampledemo/src/main/java/com/sample/yl/sampledemo/shape/ShapeActivity.java
@@ -3,7 +3,9 @@ package com.sample.yl.sampledemo.shape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.hjq.shape.view.ShapeButton;
 import com.sample.yl.sampledemo.R;
 
 public class ShapeActivity extends AppCompatActivity {
@@ -12,5 +14,24 @@ public class ShapeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape);
+
+        ShapeButton shapeButton = findViewById(R.id.btn_main_test);
+        shapeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                shapeButton.getShapeDrawableBuilder()
+                        .setSolidColor(0xFF000000)
+                        .setStrokeColor(0xFF5A8DDF)
+                        .intoBackground();
+
+                shapeButton.getTextColorBuilder()
+                        .setTextColor(0xFFFFFFFF)
+                        .intoTextColor();
+
+                shapeButton.setText("颜色已经改变啦");
+            }
+        });
+
     }
 }
